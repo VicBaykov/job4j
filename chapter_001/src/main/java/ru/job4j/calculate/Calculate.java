eca851ebcf8c41b5aca0819b9318db5f381d2b5c
Victor Baykov <vic.baykov89@gmail.com>
2018-07-18 23:59:42 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,6 +7,8 @@ package ru.job4j.calculate;
 *Calculate.
 *
 *@author Victor Baykov (vic.baykov89@gmail.com)
+*@version $Id$
+*@since 0.1
 */
 public class Calculate{
 	/*
@@ -15,5 +17,13 @@ public class Calculate{
 	*/
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+	}
+	/**
+	*Method echo.
+	*@param name Your name
+	*@return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
