72e0e38db9386389ccbf3d77a8e95439ff4a46e7
Victor Baykov <vic.baykov89@gmail.com>
2018-07-18 23:17:32 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
+/*
+*Calculator.
+*/
 package ru.job4j.calculate;
 
-
+/*
+*Calculate.
+*
+*@author Victor Baykov (vic.baykov89@gmail.com)
+*/
 public class Calculate{
+	/*
+	*Main.
+	*@param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 	}
