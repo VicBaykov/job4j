10c7076f7565be3fcc1a485a39a38468d085638a
Victor Baykov <vic.baykov89@gmail.com>
2018-07-27 09:29:31 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,29 @@
+/*
+*Test.
+*/
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/*
+*Test.
+*
+*@author Victor Baykov (vic.baykov89@gmail.com)
+*@version $Id$
+*@since 0.1
+*/
+public class CounterTest {
+
+	@Test
+	public void whenSumEvenNumbersFromOneToTenThenThirty() {
+		Counter counter = new Counter();
+		int result = counter.add(1, 10);
+		assertThat(result, is(30));
+	}
+
+
+}
