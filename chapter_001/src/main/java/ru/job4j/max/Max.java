09d2a61f868c4b15c5e669601e631903773ed387
Victor Baykov <vic.baykov89@gmail.com>
2018-07-28 09:04:32 +0300

    4.4. Максимум из трех чисел [#190] Удалил переменную max;

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -4,13 +4,11 @@ package ru.job4j.max;
  * Created by vicba on 25.07.2018.
  */
 public class Max {
-    private int max;
     public int max(int first, int second) {
         return (first > second) ? first : second;
     }
 
     public int max(int a, int b, int c) {
-        max = max(max(a, b), c);
-        return max;
+        return max(max(a, b), c);
     }
 }
