61117902fc5afcb9216f31e2e23a32b0b1f49924
Victor Baykov <vic.baykov89@gmail.com>
2018-07-29 22:30:34 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+/**
+ * Created by vicba on 29.07.2018.
+ */
+public class FindLoop {
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Created by vicba on 29.07.2018.
+ */
+public class FindLoopTest {
+    FindLoop find = new FindLoop();
+    int[] input = new int[] {5, 10, 3};
+    @Test
+    public void whenArrayHasLengh5Then0() {
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    public void whenArrayHasLengh5ThenNone() {
+        int value = 6;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+
+}
