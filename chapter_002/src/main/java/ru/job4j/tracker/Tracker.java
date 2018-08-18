7cab2431e586f4fe6975091dadca8a409cab3ce2
Victor Baykov <vic.baykov89@gmail.com>
2018-08-18 15:17:50 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения [#784] Исправлен баг с появлением дубликатов при удалении.

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -12,12 +12,23 @@ public class Tracker {
 
     private static final Random RND = new Random();
 
+    /**
+     * Добавляет элемент
+     * @param item новый добавляемый элемент
+     * @return Возвращает добавленный элемент.
+     */
     public Item add(Item item) {
         item.setId(this.generateId());
         this.items[this.pos++] = item;
         return item;
     }
 
+    /**
+     * Редактирует элемент с выбранным id
+     * @param id идентификатор редактируемого элемента.
+     * @param item редактируемый элемент
+     * @return Возвращает true если редактирование завершено успешно, false - если нет.
+     */
     public boolean replace(String id, Item item) {
         boolean result = false;
         for (int i = 0; i < this.pos; i++) {
@@ -31,6 +42,11 @@ public class Tracker {
         return result;
     }
 
+    /**
+     * Удаляет элемент с выбранным id
+     * @param id идентификатор редактируемого элемента.
+     * @return Возвращает true если удаление завершено успешно, false - если нет.
+     */
     public boolean delete(String id) {
         boolean result = false;
         for (int i = 0; i < this.items.length; i++) {
@@ -44,10 +60,19 @@ public class Tracker {
         return result;
     }
 
+    /**
+     * Возвращает массив всех элементов
+     * @return Возвращает массив всех не null элементов
+     */
     public Item[] findAll() {
         return Arrays.copyOf(this.items, pos);
     }
 
+    /**
+     * Возвращает массив всех элементов с выбранным именем
+     * @param key имя по которому надо произвести выборку.
+     * @return Возвращает массив всех элементов с именем key
+     */
     public Item[] findByName(String key) {
         Item[] itemByName = new Item[items.length];
         int i = 0;
@@ -59,6 +84,11 @@ public class Tracker {
         return itemByName;
     }
 
+    /**
+     * Возвращает элемент с выбранным id
+     * @param id идентификатор по которому надо произвести выборку.
+     * @return Возвращает элемент с выбранным id
+     */
     public Item findById(String id) {
         Item itemTmp = null;
         for (Item item : items) {
@@ -69,6 +99,10 @@ public class Tracker {
         return itemTmp;
     }
 
+    /**
+     * Генерирует уникальный идентификатор id
+     * @return Возвращает уникальный id
+     */
     private String generateId() {
         return String.valueOf(System.currentTimeMillis() * RND.nextInt());
     }
